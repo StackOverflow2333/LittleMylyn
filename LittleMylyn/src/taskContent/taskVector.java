6ea2d25684f724499cc68130eb4154793f287968
lyz-clarence <14302010020@fudan.edu.cn>
2016-05-31 11:25:49 +0800

    LYZ第四次更新taskVector框架

---
+++ LittleMylyn/src/taskContent/taskVector.java
@@ -0,0 +1,21 @@
+package taskContent;
+
+import java.util.Vector;
+
+public class taskVector extends Vector<Task>
+{
+	//读取当前全部task
+	public taskVector(){
+		
+	}
+	
+	//得到激活的Task编号,没有则返回-1
+	public int getActivity(){
+		return -1;
+	}
+	
+	//删除第i个Task
+	public void delete(int i){
+		
+	}
+}
